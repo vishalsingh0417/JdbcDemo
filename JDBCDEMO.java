75041155cac7b10657231ad3856960ed539c3492
Vishal Kumar <82105262+vishalsingh0417@users.noreply.github.com>
2021-04-18 12:45:02 +0530

    Update JDBCDEMO.java

--- JDBCDEMO.java
+++ JDBCDEMO.java
@@ -18,28 +18,28 @@ public class JDBCDEMO {
 	Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbcdemo?useSSL=false","root","root");
 	Statement stmt=con.createStatement();	
 	
-//	//Insertion Operation
-//	String query="Insert into persons values(101,'Singh','Vishal','Bihar','Gaya')";
-//	stmt.executeUpdate(query);
-//	String query1="Insert into persons values(102,'Kumar','Ram','Jharkhand','Bokaro')";
-//	stmt.executeUpdate(query1);
-//	String query2="Insert into persons values(103,'Choudhary','Raju','Jharkhand','Ranchi')";
-//	stmt.executeUpdate(query2);
-//	String query3="Insert into persons values(104,'Suri','Rajesh','Bihar','Sitamarhi')";
-//	stmt.executeUpdate(query3);
-//	String query4="Insert into persons values(105,'Choudhary','Nagendra','Bihar','Gopalganj')";
-//	stmt.executeUpdate(query4);
-//	
-//	
-//	//Updation Operation
-//	String up="Update persons set FirstName='Abhinav' where PersonID=103";
-//	stmt.executeUpdate(up);
-//	String up1="Update persons set LastName='Yadav' where PersonID=105";
-//	stmt.executeUpdate(up1);
-//	
-//	//deletion Operation
-//	String sql="delete from persons where PersonID=102";
-//	stmt.executeUpdate(sql);
+	//Insertion Operation
+	String query="Insert into persons values(101,'Singh','Vishal','Bihar','Gaya')";
+	stmt.executeUpdate(query);
+	String query1="Insert into persons values(102,'Kumar','Ram','Jharkhand','Bokaro')";
+	stmt.executeUpdate(query1);
+	String query2="Insert into persons values(103,'Choudhary','Raju','Jharkhand','Ranchi')";
+	stmt.executeUpdate(query2);
+	String query3="Insert into persons values(104,'Suri','Rajesh','Bihar','Sitamarhi')";
+	stmt.executeUpdate(query3);
+	String query4="Insert into persons values(105,'Choudhary','Nagendra','Bihar','Gopalganj')";
+	stmt.executeUpdate(query4);
+	
+	
+	//Updation Operation
+	String up="Update persons set FirstName='Abhinav' where PersonID=103";
+	stmt.executeUpdate(up);
+	String up1="Update persons set LastName='Yadav' where PersonID=105";
+	stmt.executeUpdate(up1);
+	
+	//deletion Operation
+	String sql="delete from persons where PersonID=102";
+	stmt.executeUpdate(sql);
 
 	
 	String out="Select * from persons";
